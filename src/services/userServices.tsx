38ca6b28085650dd9040a454be7629e6ed46b79e
pawanlaksahan <pawanlaksahan10@gmail.com>
2026-02-22 14:27:55 +0530

    Create user authentication process

--- src/services/userServices.tsx
+++ src/services/userServices.tsx
@@ -3,15 +3,20 @@ import axios from "axios";
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const postSocialToken = async (token: string, provider: 'google' | 'meta') => {
-    const endpoint = "/api/user/socialLoginValidate";
+    const endpoint = provider === 'google' 
+        ? "api/user/googleLoginValidate" 
+        : "api/user/metaLoginValidate";
     try {
         const response = await axios.post(baseUrl + endpoint, {
-            tokenId: token,
+            tokenId: token ,
             provider: provider
         });
+        if (response.data.token) {
+            localStorage.setItem('token', response.data.token);
+        }
         return response.data;
     } catch (error) {
         console.error(`Error during ${provider} authentication:`, error);
         throw error;
-    }
+    } 
 };
